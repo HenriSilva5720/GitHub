ad117a4c1dcfb33115c9e2e98f7244c2bbb59248
henrique <henrique.da.silva5720@gmail.com>
2022-03-31 23:13:54 -0300

    feat: Added logo redirect to landing page

--- components/header.tsx
+++ components/header.tsx
@@ -1,15 +1,20 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
 import github_icon from "../public/assets/github_icon.svg";
 import styles from "./header.module.css";
 
 export default function Header() {
+  const router = useRouter();
+
   return (
     <header className={styles.header}>
       <div className={styles.container}>
-        <picture className={styles.image}>
-          <Image src={github_icon} alt="github-logo" />
-        </picture>
-        <h1 className={styles.title}>GitHub</h1>
+        <div className={styles.logo} onClick={() => router.push("/")}>
+          <picture className={styles.image}>
+            <Image src={github_icon} alt="github-logo" />
+          </picture>
+          <h1 className={styles.title}>GitHub</h1>
+        </div>
       </div>
     </header>
   );
