a44e64d60089589f493f64ca32df8f31aedf9c5a
henrique <henrique.da.silva5720@gmail.com>
2022-04-01 16:21:55 -0300

    feat: github api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,29 @@
 import Image from "next/image";
+import { useState } from "react";
 import Header from "../components/header";
 import styles from "./home.module.css";
 import github_img from "../public/assets/github_img.svg";
+import api from "../services/api";
 
 export default function HomePage() {
+  const [input, setInput] = useState("");
+
+  const [org, setOrg] = useState({});
+
+  async function handleClick() {
+    if (!input) return;
+
+    const res = await api
+      .get(`/orgs/${input}`)
+      .catch((err) => console.log(err));
+
+    setInput("");
+
+    if (!res?.data) return;
+
+    setOrg(res.data);
+  }
+
   return (
     <>
       <Header />
@@ -16,12 +36,19 @@ export default function HomePage() {
             platform in the world.
           </p>
           <div className={styles.inputContainer}>
-            <input className={styles.input} placeholder="Search your org" />
-            <button className={styles.button}>Search</button>
+            <input
+              className={styles.input}
+              placeholder="Search your org"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+            <button className={styles.button} onClick={handleClick}>
+              Search
+            </button>
           </div>
         </div>
         <div className={styles.containerImage}>
-          <picture className={styles.image}>
+          <picture>
             <Image src={github_img} alt="github-image" />
           </picture>
         </div>
