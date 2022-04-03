84498a30579dfa58e6141a824063bb2571ebdd08
henrique <henrique.da.silva5720@gmail.com>
2022-04-02 22:08:49 -0300

    feat: organization page mobile

---
+++ src/pages/org/[orgName].tsx
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import Link from "next/link";
+import Header from "../../components/header";
+import { useOrganization } from "../../providers/Organization";
+import styles from "./organization.module.css";
+
+export default function OrganizationPage() {
+  const { organization } = useOrganization();
+
+  return (
+    <>
+      <Header />
+      <main className={styles.container}>
+        <div className={styles.containerInfos}>
+          <picture>
+            <Image
+              className={styles.image}
+              src={organization.avatar_url}
+              alt={`${organization.name}_img`}
+              width="100px"
+              height="100px"
+            />
+          </picture>
+          <div className={styles.containerContent}>
+            <div className={styles.containerTitle}>
+              <h1>{organization.name}</h1>
+            </div>
+            <p className={styles.description}>{organization.description}</p>
+            <div className={styles.containerSocial}>
+              <span>{organization.location}</span>
+              <Link
+                href={`https://twitter.com/${organization.twitter_username}`}
+                passHref
+              >
+                <a
+                  className={styles.social}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >{`@${organization.twitter_username}`}</a>
+              </Link>
+              <Link href={organization.blog} passHref>
+                <a target="_blank" rel="noopener noreferrer">
+                  {organization.blog}
+                </a>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
